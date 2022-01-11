238f4fe4e64f4cd103c232d1d538007de9fe19c3
panda404 <yakhyoev.temur@mail.ru>
2022-01-11 23:45:18 +0400

    LeetCode Problems MergeSortedArray using bubble sort

---
+++ src/leetcode/MergeSortedArray.java
@@ -0,0 +1,29 @@
+package leetcode;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class MergeSortedArray {
+
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+
+        for (int i = (m+n)-1; i >= m; i--) {
+            nums1[i] = nums2[--n];
+        }
+        //Arrays.sort(nums1);
+        boolean state=true;
+
+        while(state) {
+            state=false;
+            for (int i = 0; i < nums1.length; i++) {
+                if (i<nums1.length-1 && nums1[i] > nums1[i + 1]) {
+                    int temp = nums1[i];
+                    nums1[i] = nums1[i + 1];
+                    nums1[i + 1] = temp;
+                    state=true;
+                }
+            }
+        }
+    }
+}
